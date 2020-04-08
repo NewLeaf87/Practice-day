cb85073f20866af0e921aefb44daf28397c18788
larell mcbride <mcbridelarell@gmail.com>
2020-04-08 10:08:25 -0400

    caps

--- cap.js
+++ cap.js
@@ -2,7 +2,8 @@ let char = ['mario','yoshi','dk']
 
 
 for (let i = 0; i < char.length; i++) {
-    if (typeof stock[i] === 'string') {
+    if (typeof char[i] === 'string') {
         char[i] = char[i].toUpperCase();
+        console.log(char[i])
     }
 }
